6e79eb8fe09e0a72dcea520a8896b78882a50c84
Ashshoffner1 <amshoffner936@access.alamancecc.edu>
2024-12-16 13:38:16 -0500

    Add files via upload

---
+++ Assignment 2/assignment_2.py
@@ -0,0 +1,42 @@
+# Please answer each question and ensure each is answered as throughout as possible.
+# There is no minimum word requirment or maximum. Comprehension is key.
+
+## --> TODO: Question 1
+#What is virtual environment? How are these virtual environments used in programming and why are they important?
+#It's a isolated workspace within a machine.They are used to work on projects in a controlled space and separates projects
+## --> TODO: Question 2
+#What is difference between the configurations of a virtual environment and the configs of your global machine?
+#virtual configs are isolated while global are not
+## --> TODO: Question 3
+#What is versión control in programming? Describe the process which is followed?
+#organizing files in a virtual environment
+## --> TODO: Question 4
+#Describe what the following commands do:
+# <git init> initialize empty repo
+# <git remote add origin> makes a clone of or repo on git hub
+# <git pull origin main> pulls it to local machine
+# <git add (for names)> adds a name
+# <git commit -m “comment here”< to document what you did
+# <git checkout> switches branch
+# <git status> shows the working tree status
+# <git remote -v> checks location of repo
+# <git branch -a> lists branches
+
+## --> TODO: Question 5
+#What are ssh keys? Why are these important and describe the process of creating these on your local machine?
+#special generated keys for a local machine used for authenticating connections
+#use git bash to enter commands to generate keys for your local machine
+## --> TODO: Question 6
+#What is terminal/command prompt used for? Why may these be important to engineers?
+#entering commands in the command line. they can trouble shoot many things using the terminal
+## --> TODO: Question 7
+# Describe what these linux commands do:
+# <ls> list flies
+# <cd> change directories
+# <mkdir> creates directory
+# <touch>makes empty file
+# <cd>change directories
+# <cp> copying flies
+# <mv>rename and move
+# <cat>displays the content
+# <tail>prints last few numbers
